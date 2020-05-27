ea2d8cca502c984ae4cede5c80dd6007c04baf9d
LautaroParada <lautaro.parada.opazo@gmail.com>
2020-05-27 19:59:01 -0400

    Create test.m
    way to correlate the variables

---
+++ test.m
@@ -0,0 +1,30 @@
+clear; clc;
+nObs = 100;
+rng(randi([1 100]))
+size0 = 5; % uncorrlated variables
+size1 = 5; % correlated variables
+sigma1 = 0.25; %volatility for the random correlated noise
+
+% creating correlation between the variables
+approach = '1';
+
+if strcmp(approach, '1')
+    % generating some uncorrelated data
+    x = randn(nObs, size0);
+    cols = randi([1 size0-1], 1, size1);
+    y = x(:, cols) + sigma1.*randn(nObs, numel(cols));
+    x_ = [x y];
+    r = cov(x_);
+    c = chol(r, 'lower');
+    x_ = (c * x_')';
+else
+    % generating some uncorrelated data
+    x = randn(nObs, size0 + size1);
+    r = cov(x);
+    c = chol(r, 'lower');
+    x_ = (c * x')';
+end
+
+% consolidating everythin in just one dataset
+% heatmap(corrcoef(x_))
+plotmatrix(x_)
